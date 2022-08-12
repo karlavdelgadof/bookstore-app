1aa5998382568a68be73ae1a5bd9bae78af286d4
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-12 18:34:41 -0400

    Create functional components for the app

---
+++ src/components/BookItem.js
@@ -0,0 +1,32 @@
+
+function BookItem(props) {
+  const { title, genre, percentage, author, chapter } = props.book;
+  return (
+    <div className="book">
+      <div className="b-info">
+        <span>{ genre }</span>
+        <h2>{ title }</h2>
+        <h3>{ author }</h3>
+        <div className='actions-container'>
+          <button type="button" className="btn" id="comment">Comments</button>
+          <button type="button" className="btn" id="comment">Remove</button>
+          <button type="button" className="btn" id="comment">Edit</button>
+        </div>
+      </div>
+
+      <div className="progress-percent">
+        <h3>{ percentage }</h3>
+        <span>Completed</span>
+      </div>
+
+      <div className="update-progress">
+        <span>Current Chapter</span>
+        <h2>{ chapter }</h2>
+        <button type="button">Update progress</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default BookItem;
---
+++ src/pages/Categories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Categories() {
+  return (
+    <button type="button" id="check-status">Check status</button>
+  );
+}
+
+export default Categories;
