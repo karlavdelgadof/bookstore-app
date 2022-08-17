81395d019318ab5d6c9c76145389eabf03fbfd42
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-17 16:46:14 -0400

    Implement useSelector to consume books array

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,40 +1,13 @@
-import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 import Form from './Form';
 
-const books = [
-  {
-    id: 1,
-    title: 'The Hunger Games',
-    genre: 'Action',
-    percentage: 64,
-    author: 'Suzanne Collins',
-    chapter: 17,
-  },
-  {
-    id: 2,
-    title: 'Dune',
-    genre: 'Sci-Fi',
-    percentage: 35,
-    author: 'Frank Herbert',
-    chapter: 3,
-  },
-  {
-    id: 3,
-    title: 'Capital in the Twenty-First Century',
-    genre: 'Economy',
-    percentage: 18,
-    author: 'Suzanne Collins',
-    chapter: 8,
-  },
-];
-
-function BooksContainer() {
-  const [state] = useState(books);
+export default function BooksContainer() {
+  const books = useSelector((state) => state.books);
   return (
     <>
       <div id="books-list">
-        {state.map((book) => (
+        {books.map((book) => (
           <BookItem key={book.id} book={book} />
         ))}
       </div>
@@ -42,5 +15,3 @@ function BooksContainer() {
     </>
   );
 }
-
-export default BooksContainer;
