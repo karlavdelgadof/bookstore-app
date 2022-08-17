7a2eb723533bd88eb90bac6ba3e8be6d74c16199
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-17 17:06:17 -0400

    Implement useDispatch for add button and useState for data input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
 function Form() {
+  const dispatch = useDispatch();
+  const [bTitle, setTitle] = useState('');
+  const [bAuthor, setAuthor] = useState('');
+
   return (
     <div id="add-book">
       <h1>ADD NEW BOOK</h1>
       <form id="form-book">
-        <input id="title" placeholder="Book title" type="text" />
-        <input id="author" placeholder="Author" type="text" />
-        <button type="button">ADD BOOK</button>
+        <input id="title" value={bTitle} onInput={(e) => setTitle(e.target.value)} placeholder="Book title" type="text" />
+        <input id="author" value={bAuthor} onInput={(e) => setAuthor(e.target.value)} placeholder="Author" type="text" />
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(addBook({
+              title: bTitle,
+              author: bAuthor,
+              id: uuidv4(),
+            }));
+            setAuthor('');
+            setTitle('');
+          }}
+        >
+          ADD BOOK
+        </button>
       </form>
     </div>
   );
