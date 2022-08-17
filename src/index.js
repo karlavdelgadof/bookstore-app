06f2ee8aed22b55c6abfee1668baad91d962d633
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-17 16:40:15 -0400

    Import Provider and store to wrap main app

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import BooksContainer from './components/BooksContainer';
 import Categories from './pages/Categories';
+
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
+        <Route
+          path="/"
+          element={(
+            <Provider store={store}>
+              <App />
+            </Provider>
+        )}
+        >
           <Route path="/" element={<BooksContainer />} />
           <Route path="/categories" element={<Categories />} />
         </Route>
