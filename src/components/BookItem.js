4966a5a9200f9921d28ce1cacdca5492c1d7d406
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-17 17:10:23 -0400

    Import useDispatch to dispatch remove action creator

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,18 +1,29 @@
-/* eslint-disable react/prop-types,  react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 function BookItem(props) {
   const {
     book,
   } = props;
+  const dispatch = useDispatch();
   return (
-    <div className="book">
+    <div className="book" id={book.id}>
       <div className="b-info">
         <span>{ book.genre }</span>
         <h2>{ book.title }</h2>
         <h3>{ book.author }</h3>
         <div className="actions-container">
           <button type="button" className="btn" id="comment">Comments</button>
-          <button type="button" className="btn" id="comment">Remove</button>
+          <button
+            type="button"
+            className="btn"
+            id="comment"
+            onClick={() => dispatch(removeBook(book))}
+          >
+            Remove
+          </button>
           <button type="button" className="btn" id="comment">Edit</button>
         </div>
       </div>
