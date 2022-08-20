c7f45aadfb9b3bb72a15fd9c58902001c33e2398
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-20 18:57:08 -0400

    Add container for the categories view

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { checkStatus } from '../redux/categories/categories';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { checkStatus } from "../redux/categories/categories";
 
 function Categories() {
   const dispatch = useDispatch();
   return (
-    <button
-      type="button"
-      id="check-status"
-      onClick={() => dispatch(checkStatus())}
-    >
-      Check status
-    </button>
+    <div id="categories-sec">
+      <button
+        type="button"
+        id="check-status"
+        onClick={() => dispatch(checkStatus())}
+      >
+        Check status
+      </button>
+    </div>
   );
 }
 
