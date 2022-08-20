96c9e6b75227cf9c134888c9950d7dd199a1f9fd
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-20 19:04:06 -0400

    Fix eslint errors

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { checkStatus } from "../redux/categories/categories";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 function Categories() {
   const dispatch = useDispatch();
